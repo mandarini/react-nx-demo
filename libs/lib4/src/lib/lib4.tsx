c8aae33c90f7c4bd8b6a54210fd975f6d384af50
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-05-30 10:21:51 +0300

    connected libs

--- libs/lib4/src/lib/lib4.tsx
+++ libs/lib4/src/lib/lib4.tsx
@@ -1,9 +1,13 @@
 import styles from './lib4.module.css';
+import { Lib1 } from '@react-demo/lib1';
+import { Lib2 } from '@react-demo/lib2';
 
 export function Lib4() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Lib4!</h1>
+      <Lib1 />
+      <Lib2 />
     </div>
   );
 }
--- libs/ui/lib2/src/lib/lib2.tsx
+++ libs/ui/lib2/src/lib/lib2.tsx
@@ -1,9 +1,11 @@
 import styles from './lib2.module.css';
+import { Lib3 } from '@react-demo/lib3';
 
 export function Lib2() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Lib2!</h1>
+      <Lib3 />
     </div>
   );
 }
