709b3738020c554ea6cc6454e766c9286b1be7f4
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-05-30 10:24:41 +0300

    connected all libs

--- apps/myapp/app/app.tsx
+++ apps/myapp/app/app.tsx
@@ -1,11 +1,17 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
+import { Lib4 } from '@react-demo/lib4';
+import { ulib1 } from '@react-demo/ulib1';
+import { ulib3 } from '@react-demo/ulib3';
 
 export function App() {
+  console.log(ulib1());
+  console.log(ulib3());
   return (
     <div>
       <NxWelcome title="@react-demo/myapp" />
+      <Lib4></Lib4>
     </div>
   );
 }
--- libs/utils/ulib1/src/lib/ulib1.ts
+++ libs/utils/ulib1/src/lib/ulib1.ts
@@ -1,3 +1,6 @@
+import { ulib3 } from '@react-demo/ulib3';
+
 export function ulib1(): string {
+  console.log('ulib3', ulib3());
   return 'ulib1';
 }
