8003913538dab58f541b32fb542bea3118bc76f9
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-05-30 10:19:17 +0300

    fixed libs

---
+++ libs/lib4/src/lib/lib4.tsx
@@ -0,0 +1,11 @@
+import styles from './lib4.module.css';
+
+export function Lib4() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Lib4!</h1>
+    </div>
+  );
+}
+
+export default Lib4;
